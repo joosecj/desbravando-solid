4dc1db769585a5ccd625ccffa2e0cf671e2b3c0b
Jose Carlos <josecarloscjj@gmail.com>
2023-03-31 18:03:38 -0300

    criando abstrações para cada class de baixo nivel

--- cotuba/src/main/java/cotuba/application/Cotuba.java
+++ cotuba/src/main/java/cotuba/application/Cotuba.java
@@ -3,15 +3,16 @@ package cotuba.application;
 import cotuba.domain.Capitulo;
 import cotuba.domain.Ebook;
 import cotuba.epub.*;
-import cotuba.md.RenderizadorMDParaHTML;
+import cotuba.md.RenderizadorMDParaHTMLImpl;
 import cotuba.pdf.GeradorPDF;
+import cotuba.pdf.GeradorPDFImpl;
 
 import java.nio.file.Path;
 import java.util.List;
 
 public class Cotuba {
     public void executa(Path diretorioDosMD, String formato, Path arquivoDeSaida) {
-        var renderizadorMDParaHTML = new RenderizadorMDParaHTML();
+        var renderizadorMDParaHTML = new RenderizadorMDParaHTMLImpl();
         List<Capitulo> capitulos = renderizadorMDParaHTML.renderiza(diretorioDosMD);
 
         var ebook = new Ebook();
@@ -21,10 +22,10 @@ public class Cotuba {
 
 
         if ("pdf".equals(formato)) {
-            var geradorPDF = new GeradorPDF();
+            GeradorPDF geradorPDF = new GeradorPDFImpl();
             geradorPDF.gera(ebook);
         } else if ("epub".equals(formato)) {
-            var geradorEPUB = new GeradorEPUB();
+            GeradorEPUB geradorEPUB = new GeradorEPUBImpl();
             geradorEPUB.gera(ebook);
         } else {
             throw new IllegalArgumentException("Formato do ebook inválido: " + formato);
---
+++ cotuba/src/main/java/cotuba/epub/GeradorEPUBImpl.java
@@ -0,0 +1,35 @@
+package cotuba.epub;
+
+import cotuba.domain.Capitulo;
+import cotuba.domain.Ebook;
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.Resource;
+import nl.siegmann.epublib.epub.EpubWriter;
+import nl.siegmann.epublib.service.MediatypeService;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class GeradorEPUBImpl implements GeradorEPUB {
+
+    @Override
+    public void gera(Ebook ebook) {
+        var arquivoDeSaida = ebook.getArquivoDeSaida();
+        var epub = new Book();
+
+        var epubWriter = new EpubWriter();
+
+        for (Capitulo capitulo : ebook.getCapitulos()) {
+            String titulo = capitulo.getTitulo();
+            String html = capitulo.getConteudoHTML();
+            // TODO: usar título do capítulo
+            epub.addSection(titulo, new Resource(html.getBytes(), MediatypeService.XHTML));
+        }
+
+        try {
+            epubWriter.write(epub, Files.newOutputStream(arquivoDeSaida));
+        } catch (IOException ex) {
+            throw new IllegalStateException("Erro ao criar arquivo EPUB: " + arquivoDeSaida.toAbsolutePath(), ex);
+        }
+    }
+}
