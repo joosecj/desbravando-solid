eb6f89fab7171f21f5116e99ab632c367fb5784f
Jose Carlos <josecarloscjj@gmail.com>
2023-04-01 11:16:51 -0300

    removendo a factory que chama a implmentação

--- cotuba/src/main/java/cotuba/application/GeradorPDF.java
+++ cotuba/src/main/java/cotuba/application/GeradorPDF.java
@@ -1,12 +1,8 @@
 package cotuba.application;
 
 import cotuba.domain.Ebook;
-import cotuba.pdf.GeradorPDFComIText;
 
 public interface GeradorPDF {
     void gera(Ebook ebook);
 
-    static GeradorPDF cria() {
-        return new GeradorPDFComIText();
-    }
 }
