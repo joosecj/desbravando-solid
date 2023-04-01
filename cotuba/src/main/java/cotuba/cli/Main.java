3c1b0844a4c2883db92a534c4475653ec228ec3f
Jose Carlos <josecarloscjj@gmail.com>
2023-04-01 11:22:05 -0300

    Spring controlar atraves da Class CotubaConfig e faz a injecão

--- cotuba/src/main/java/cotuba/cli/Main.java
+++ cotuba/src/main/java/cotuba/cli/Main.java
@@ -1,6 +1,9 @@
 package cotuba.cli;
 
 import cotuba.application.Cotuba;
+import cotuba.CotubaConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.nio.file.Path;
 
@@ -17,8 +20,10 @@ public class Main {
             formato = opcoesCLI.getFormato();
             arquivoDeSaida = opcoesCLI.getArquivoDeSaida();
             modoVerboso = opcoesCLI.isModoVerboso();
-             var cotuba = new Cotuba();
-             cotuba.executa(opcoesCLI);
+            ApplicationContext applicationContext = new AnnotationConfigApplicationContext(CotubaConfig.class);
+            Cotuba cotuba = applicationContext.getBean(Cotuba.class);
+
+            cotuba.executa(opcoesCLI);
             System.out.println("Arquivo gerado com sucesso: " + arquivoDeSaida);
 
         } catch (Exception ex) {
